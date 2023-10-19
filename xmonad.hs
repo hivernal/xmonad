3f515bbeba90b7ff840f9a80cfa885ace5dc54ff
hivernal <geryaevna@mail.ru>
2023-10-19 16:56:51 +0700

    change xmobar font

--- xmonad.hs
+++ xmonad.hs
@@ -4,26 +4,22 @@ import System.Exit
 import XMonad.Actions.DwmPromote
 
 import XMonad.Util.EZConfig
-import XMonad.Util.Ungrab
-import XMonad.Util.Loggers
 import XMonad.Util.WorkspaceCompare
 import XMonad.Util.Cursor
 
 import XMonad.Layout.Spacing
-import XMonad.Layout.NoBorders 
+import XMonad.Layout.NoBorders
 
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Hooks.Place
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
 
 myXmobarPP :: PP
 myXmobarPP = def
-    { ppCurrent         = xmobarColor "#fcfcfc" "#646870" . wrap " " " " 
+    { ppCurrent         = xmobarColor "#fcfcfc" "#5c6667" . wrap " " " "
     , ppHidden          = xmobarColor "#fcfcfc" "" . wrap " " " "
-		, ppHiddenNoWindows = xmobarColor "#646870" "" . wrap " " " "
+		, ppHiddenNoWindows = xmobarColor "#5c6667" "" . wrap " " " "
     , ppSep             = " "
     , ppTitle           = xmobarColor "#fcfcfc" "" .shorten 50
     , ppLayout  = (\ x -> case x of
@@ -32,8 +28,8 @@ myXmobarPP = def
         _               -> x )
     }
 
-myLayout = lessBorders OnlyFloat (smartSpacingWithEdge 5 (smartBorders tiled) ||| noBorders Full)
--- myLayout = smartSpacingWithEdge 5 (smartBorders (withBorder 3 tiled)) |||  Full
+-- myLayout = lessBorders OnlyFloat (smartSpacingWithEdge 5 (smartBorders tiled) ||| noBorders Full)
+myLayout = lessBorders OnlyFloat (spacingWithEdge 4 (tiled |||  Full))
   where
     tiled   = Tall nmaster delta ratio
     nmaster = 1      -- Default number of windows in the master pane
@@ -44,19 +40,19 @@ myPlacement = fixed (0.5,0.5)
 
 autostart :: X ()
 autostart = do
-  -- spawn "picom -b"
+  spawn "picom -b"
   spawn "lxpolkit &"
-  spawn "feh --bg-max /home/nikita/pictures/astronavt.jpg"
+  spawn "feh --bg-max /home/nikita/pictures/neboskreb.jpg"
   setDefaultCursor xC_left_ptr
 
 main :: IO ()
-main = xmonad 
+main = xmonad
     . ewmhFullscreen
     . ewmh
     -- . setEwmhWorkspaceSort getSortByXineramaRule
     -- . withEasySB (statusBarProp "xmobar ~/.config/xmonad/xmobarrc.icons" (pure myXmobarPP)) defToggleStrutsKey
     . withEasySB (statusBarProp "polybar" (pure def)) defToggleStrutsKey
-    $ myConfig 
+    $ myConfig
 
 
 myConfig = def
@@ -70,15 +66,15 @@ myConfig = def
   , startupHook = autostart
   }
 
-  `additionalKeysP` 
+  `additionalKeysP`
     [ ("M-p", spawn "dmenu_run")
     , ("M-q", kill)
     , ("M-S-r", spawn "if type xmonad; then xmonad --recompile && xmonad --restart; else xmessage xmonad not in \\$PATH: \"$PATH\"; fi")
     , ("M-<Tab>", sendMessage NextLayout)
     , ("M-S-q", spawn "shutdown -h now")
     , ("M-S-e",  io exitSuccess)
-    , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 5")
-    , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5")
+    , ("<XF86MonBrightnessUp>", spawn "sudo xbacklight -inc 5")
+    , ("<XF86MonBrightnessDown>", spawn "sudo xbacklight -dec 5")
     -- , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 5%-")
     -- , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 5%+")
     , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -5%")
